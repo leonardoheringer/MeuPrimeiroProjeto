7c22284d9f364bd75cf52232fefc507736b146de
Leonardo Heringer <heringerleonardo560@gmail.com>
2024-09-18 23:50:12 -0300

    Jogo C#

---
+++ PedraPapelTesoura/Program.cs
@@ -0,0 +1,30 @@
+﻿Random random = new Random();
+        string[] opções = { "Pedra", "Papel", "Tesoura" };
+
+        while (true)
+        {
+            Console.Write("Escolha (Pedra, Papel ou Tesoura ): ");
+            string escolhaJogador = Console.ReadLine();
+
+            
+
+            if (Array.IndexOf(opções, escolhaJogador) == -1)
+            {
+                Console.WriteLine("Escolha inválida. Tente novamente.");
+                continue;
+            }
+
+            string escolhaComputador = opções[random.Next(opções.Length)];
+            Console.WriteLine($"Computador escolheu: {escolhaComputador}");
+
+            if (escolhaJogador == escolhaComputador)
+                Console.WriteLine("Empate!");
+            else if ((escolhaJogador == "Pedra" && escolhaComputador == "Tesoura") ||
+                     (escolhaJogador == "Papel" && escolhaComputador == "Pedra") ||
+                     (escolhaJogador == "Tesoura" && escolhaComputador == "Papel"))
+                Console.WriteLine("Você ganhou!");
+            else
+                Console.WriteLine("Você perdeu!");
+
+            Console.WriteLine();
+        }
